d2745e5fce64407d20a94711a111a62cef5f2144
hanbyul-choi <hanbyul1025@gmail.com>
2024-04-09 00:29:25 +0900

    feat: face & medspa 페이지 제작 완료

--- src/app/face/Chin-Augmentation/page.js
+++ src/app/face/Chin-Augmentation/page.js
@@ -1,5 +1,67 @@
 import React from 'react';
 
+import BreastSample from '../../../../public/detailpage/breast/BreastAugmentationImg.png';
+
+import AboutSecondBanner from '@/components/about/AboutSecondBanner';
+import CommonTopBanner from '@/components/common/banner/CommonTopBanner';
+import BeforeAfter from '@/components/detail/common/BeforeAfter';
+import CardContainer from '@/components/detail/common/CardContainer';
+import Section1 from '@/components/detail/common/Section1';
+import WhyChoose from '@/components/detail/common/WhyChoose';
+import Intro from '@/components/detail/nose/Intro';
+
+const images_data = ['/B&A/example.jpg', '/B&A/example.jpg', '/B&A/example.jpg', '/B&A/example.jpg'];
+
+const benefits = [
+  {
+    title: 'Improved Facial Harmony',
+    desc: 'Chin augmentation can enhance facial proportions and create a more balanced profile, bringing symmetry to your features and enhancing overall facial harmony.',
+  },
+  {
+    title: 'Enhanced Jawline Definition',
+    desc: 'A well-defined chin can accentuate the jawline, creating a more sculpted and youthful appearance.',
+  },
+  {
+    title: 'Boosted Confidence',
+    desc: 'Enjoy newfound confidence and self-assurance as you see a more proportionate and attractive reflection in the mirror.',
+  },
+  {
+    title: 'Customized Results',
+    desc: 'Whether you prefer a subtle enhancement or a more dramatic transformation, chin augmentation can be customized to achieve your desired level of enhancement, ensuring natural-looking results that complement your unique facial features.',
+  },
+];
+
 export default function Page() {
-  return <div>Page</div>;
+  const what_is =
+    'Chin augmentation, also known as genioplasty, is a surgical procedure designed to enhance the size, shape, and projection of the chin. By placing implants or reshaping the existing bone, chin augmentation can improve facial proportions, correct a weak or receding chin, and create a more aesthetically pleasing profile. Whether you desire a more prominent chin or wish to achieve better facial balance, our skilled surgeons can tailor the procedure to meet your unique needs and goals.';
+  const who_needs =
+    "Chin augmentation is an excellent option for individuals who are bothered by a weak or recessed chin, lack of definition in the jawline, or facial asymmetry. Whether you're looking to enhance your facial profile, correct a congenital deformity, or achieve a more youthful appearance, our experienced surgeons can help you achieve your aesthetic goals with precision and expertise.";
+  const whyChoose_desc =
+    'At CARA Plastic Surgery & Laser Center Las Vegas, we are committed to providing personalized care and exceptional results. Schedule a consultation with us today to learn more about how chin augmentation can help you define your profile and enhance your natural beauty.';
+
+  return (
+    <>
+      <CommonTopBanner
+        url="Face"
+        img={BreastSample}
+        title="Chin Augmentation"
+        contents="Every aspect of the face contributes to achieving facial harmony. A receding chin can detract from your profile's attractiveness and create a sense of disproportion among other features. Chin augmentation surgery offers a solution to enhance your facial appearance, restoring balance, harmony, and aesthetic appeal."
+      />
+      <Intro
+        title={'Chin Augmentation'}
+        desc={
+          "A balanced and defined chin can greatly enhance your facial profile, exuding confidence and symmetry. If you're looking to achieve a more proportionate and harmonious appearance, consider chin augmentation at CARA Plastic Surgery & Laser Center Las Vegas. Let us help you redefine your features and unleash your full potential."
+        }
+      />
+      <Section1
+        desc={what_is}
+        title={'What is Chin Augmentation?'}
+        img={'/detailpage/liposuction/liposuction_what_is.png'}
+      />
+      <CardContainer title={'Benefits of Chin Augmentation'} benefits={benefits} theme="dark" />
+      <BeforeAfter images_data={images_data} />
+      <AboutSecondBanner mainDesc="Who Can Benefit from Chin Augmentation?" subDesc={who_needs} />
+      <WhyChoose desc={whyChoose_desc} />
+    </>
+  );
 }
--- src/app/medspa/Laser-Hair-Removal/page.js
+++ src/app/medspa/Laser-Hair-Removal/page.js
@@ -1,5 +1,50 @@
 import React from 'react';
 
+import BreastSample from '../../../../public/detailpage/breast/BreastAugmentationImg.png';
+
+import AboutSecondBanner from '@/components/about/AboutSecondBanner';
+import CommonTopBanner from '@/components/common/banner/CommonTopBanner';
+import Section1 from '@/components/detail/common/Section1';
+import WhyChoose from '@/components/detail/common/WhyChoose';
+import Intro from '@/components/detail/nose/Intro';
+
 export default function Page() {
-  return <div>Page</div>;
+  const what_is = {
+    Alma_Soprano_ICE: {
+      title: 'Alma Soprano Ice',
+      desc: 'The Alma Soprano ICE is a revolutionary laser hair removal system that utilizes advanced diode laser technology to safely and effectively remove unwanted hair from virtually any part of the body. This innovative system combines cutting-edge technology with a unique cooling mechanism to ensure maximum comfort and safety during treatment.',
+      benefits: [
+        `Effective on all skin types, including tanned and dark skin.`,
+        'Virtually painless treatment with built-in cooling technology.',
+        'Fast, efficient sessions with minimal discomfort and downtime.',
+        'Long-lasting results with reduced risk of side effects.',
+      ],
+    },
+  };
+
+  const who_needs = `Laser hair removal is an ideal solution for individuals who are tired of the inconvenience and discomfort of traditional hair removal methods such as shaving, waxing, or plucking. It is suitable for both men and women and can be used to treat unwanted hair on various areas of the body, including the face, legs, arms, underarms, bikini area, and more. Whether you're looking to eliminate unwanted facial hair, achieve silky-smooth legs, or enjoy a hair-free bikini line, laser hair removal offers a safe, effective, and long-lasting solution.`;
+  const whyChoose_desc = `At CARA Plastic Surgery & Laser Center Las Vegas, our experienced team is dedicated to helping you achieve your desired results with personalized laser hair removal treatments tailored to your unique needs and skin type. Schedule a free consultation today to learn more about our laser hair removal services and start your journey to smooth, hair-free skin.`;
+
+  return (
+    <>
+      <CommonTopBanner
+        url="Medspa"
+        img={BreastSample}
+        title="Laser Hair Removal"
+        contents={`Experience the freedom of smooth, hair-free skin with our laser hair removal treatments.Our advanced laser technology targets hair follicles to inhibit future growth, leaving you with silky-smooth skin that's ready to be shown off. With quick, comfortable treatments and minimal downtime, you can enjoy the convenience of hair-free skin without the fuss.`}
+      />
+      <Intro
+        title={'Laser Hair Removal'}
+        desc={`At CARA Plastic Surgery & Laser Center, we offer advanced and comfortable hair removal treatments to help you achieve smooth, hair-free skin with lasting results. Explore our premier laser technology below to learn more about our approach to laser hair removal.`}
+      />
+      <Section1
+        desc={what_is.Alma_Soprano_ICE.desc}
+        title={`What is ${what_is.Alma_Soprano_ICE.title}?`}
+        img={'/detailpage/liposuction/liposuction_what_is.png'}
+        benefits={what_is.Alma_Soprano_ICE.benefits}
+      />
+      <AboutSecondBanner mainDesc="Who Needs Laser Hair Removal?" subDesc={who_needs} />
+      <WhyChoose desc={whyChoose_desc} />
+    </>
+  );
 }
